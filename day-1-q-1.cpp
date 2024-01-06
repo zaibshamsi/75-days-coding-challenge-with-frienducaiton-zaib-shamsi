ee5ab312182db92ec2df53ace25d37835d3ac983
zaib shamsi <135112519+zaibshamsi@users.noreply.github.com>
2024-01-06 17:05:41 +0530

    Add files via upload

---
+++ day-1-q-1.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> map;
+
+        for (int i = 0; i < nums.size(); ++i) {
+            int complement = target - nums[i];
+
+            if (map.find(complement) != map.end()) {
+                return {map[complement], i};
+            }
+
+            map[nums[i]] = i;
+        }
+        return {};
+    }
+};
